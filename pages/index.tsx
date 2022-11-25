b89a0461e59b4bda4f448a5c8a3d8a67ae25d60a
Elena Cavallero <elecav@hotmail.it>
2022-11-25 11:38:57 +0000

    change to googlemap;

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Map from "../components/Map";
+import Map from "../components/GoogleMap";
 export default function Home() {
   return (
     <div className={styles.container}>
